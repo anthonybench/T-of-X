f25c2f633f932abcf69354ecd0099c5fc939ec2d
anthonybench <anthonybenchyep@gmail.com>
2020-11-16 20:05:38 -0800

    added script to generate primary user (teacher) database entry

---
+++ backend/data/instance.js
@@ -0,0 +1,33 @@
+//===============================================================//
+/* PLACE TEACHER NAME FOR 'USER' DATABASE COLLECTION HERE */
+//===============================================================//
+const username = "Isaac Yep"; // Teacher username
+//===============================================================//
+
+
+const mongoose = require('mongoose');
+let User = require('../models/user.model');
+
+require('dotenv').config(); //enviornment variables in .env
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+const userid = 1; // start user id assignment with teacher as 1
+const useristeacher = true;
+
+const newUser = new User({
+    username : username,
+    id : userid,
+    isTeacher : useristeacher
+});
+
+try {
+    newUser.save();
+} catch(err) {
+    console.log("Please verify your teacher has been added to the 'users' database collection.");
+}
