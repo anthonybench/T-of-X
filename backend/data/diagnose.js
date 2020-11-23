0810694df978b5899fafb8b7406f922b3efd8e9a
anthonybench <anthonybenchyep@gmail.com>
2020-11-22 17:05:28 -0800

    finished diagnosing tooling, rapid endpoint testing is a go!

--- backend/data/diagnose.js
+++ backend/data/diagnose.js
@@ -2,120 +2,169 @@
 /* CONFIGURATION DATA GOES HERE */
 //===============================================================//
 const baseURI = "https://t-of-x-backend.anthonybench.vercel.app/"; // Host domain, must end with `/`
+const specificStudentSessions = 4; // Test sessions registered with particular uid
+const specificStudent = 1; // Test specific student
 //===============================================================//
 
 const https = require('https');
 const http = require('http');
-let async = require('async');
 
 let url;
+let request;
 
+// Utility: time delay
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const delay = 1000;
+
+// Utility: log error message
+function printError(error) {
+    console.error(error.message);
+}
 
 try {
-    async.waterfall([
-            function() {
-                console.log("==============Users");
-                console.log("======================================");
-                url = baseURI + 'users';
-            },
-            function() {
-                const request = https.get(url, response => {
-                    if (response.statusCode == 200) {
-                    
-                    let body = "";
-                    response.on('data', data => {
-                        body += data.toString();
-                    });
-            
-                    response.on('end', () => {
-                        try {
-                            const theResponse = JSON.parse(body);
-                            console.log(theResponse);
-                        } catch(error) {
-                            printError(error);
-                        }
-                    });
-                    } else {
-                        const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
-                        const statusCodeError = new Error(message);
-                        printError(statusCodeError);
-                    }
-                });
-                request.on('error', error => console.error(`Problem with request: ${error.message}`));
-            },
-            function() {
-                console.log("==============Sessions");
-                console.log("======================================");
-                url = baseURI + 'sessions';
-            },
-            function() {
-                const request = https.get(url, response => {
-                    if (response.statusCode == 200) {
-                    
-                    let body = "";
-                    response.on('data', data => {
-                        body += data.toString();
-                    });
-            
-                    response.on('end', () => {
-                        try {
-                            const theResponse = JSON.parse(body);
-                            console.log(theResponse);
-                        } catch(error) {
-                            printError(error);
-                        }
-                    });
-                    } else {
-                        const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
-                        const statusCodeError = new Error(message);
-                        printError(statusCodeError);
-                    }
-                });
-                request.on('error', error => console.error(`Problem with request: ${error.message}`));
-            },
-            function() {
-                console.log("==============Sessions");
-                console.log("======================================");
-                url = baseURI + 'counters';
-            },
-            function () {
-                const request = https.get(url, response => {
-                    if (response.statusCode == 200) {
-                    
-                    let body = "";
-                    response.on('data', data => {
-                        body += data.toString();
-                    });
-            
-                    response.on('end', () => {
-                        try {
-                            const theResponse = JSON.parse(body);
-                            console.log(theResponse);
-                        } catch(error) {
-                            printError(error);
-                        }
-                    });
-                    } else {
-                        const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
-                        const statusCodeError = new Error(message);
-                        printError(statusCodeError);
-                    }
-                });
-                request.on('error', error => console.error(`Problem with request: ${error.message}`));
-            },
-            function() {
-                console.log("=====Sessions for specific student");
-                console.log("======================================");
-                url = baseURI + 'sessions/';
-            }
-        ],
+    //////////////////////////////////////////////////////////
+    sleep(delay);
+    url = baseURI + 'users'; // endpoint
+    sleep(delay);
+    request = https.get(url, response => {
+        if (response.statusCode == 200) {
+        
+        let body = "";
+        response.on('data', data => {
+            body += data.toString();
+        });
 
-        function(err, result) {
-            if (!err) {
-                console.log(result);
+        response.on('end', () => {
+            try {
+                console.log("==============Users"); // console logging divider
+                console.log("======================================");
+                const theResponse = JSON.parse(body);
+                console.log(theResponse);
+            } catch(error) {
+                printError(error);
             }
+        });
+        } else {
+            const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
         }
-    );
-} catch(error) {
-    printError(error);
+    });
+    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+    //////////////////////////////////////////////////////////
+    sleep(delay);
+    url = baseURI + 'sessions'; // endpoint
+    request = https.get(url, response => {
+        if (response.statusCode == 200) {
+        
+        let body = "";
+        response.on('data', data => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            try {
+                console.log("==============Sessions");  // console logging divider
+                console.log("======================================");
+                const theResponse = JSON.parse(body);
+                console.log(theResponse);
+            } catch(error) {
+                printError(error);
+            }
+        });
+        } else {
+            const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+        }
+    });
+    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+    //////////////////////////////////////////////////////////
+    sleep(delay);
+    url = baseURI + 'counters'; // endpoint
+    request = https.get(url, response => {
+        if (response.statusCode == 200) {
+        
+        let body = "";
+        response.on('data', data => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            try {
+                console.log("==============Counters");  // console logging divider
+                console.log("======================================");
+                const theResponse = JSON.parse(body);
+                console.log(theResponse);
+            } catch(error) {
+                printError(error);
+            }
+        });
+        } else {
+            const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+        }
+    });
+    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+    //////////////////////////////////////////////////////////
+    sleep(delay);
+    url = baseURI + `sessions/${specificStudentSessions}`; // endpoint
+    request = https.get(url, response => {
+        if (response.statusCode == 200) {
+        
+        let body = "";
+        response.on('data', data => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            try {
+                console.log(`====Sessions for user ${specificStudentSessions}`);  // console logging divider
+                console.log("======================================");
+                const theResponse = JSON.parse(body);
+                console.log(theResponse);
+            } catch(error) {
+                printError(error);
+            }
+        });
+        } else {
+            const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+        }
+    });
+    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+    //////////////////////////////////////////////////////////
+    sleep(delay);
+    url = baseURI + `users/${specificStudent}`; // endpoint
+    request = https.get(url, response => {
+        if (response.statusCode == 200) {
+        
+        let body = "";
+        response.on('data', data => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            try {
+                console.log(`====Student ${specificStudent}`);  // console logging divider
+                console.log("======================================");
+                const theResponse = JSON.parse(body);
+                console.log(theResponse);
+            } catch(error) {
+                printError(error);
+            }
+        });
+        } else {
+            const message = `There was an error: ${http.STATUS_CODES[response.statusCode]}`;
+            const statusCodeError = new Error(message);
+            printError(statusCodeError);
+        }
+    });
+    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+} catch(err) {
+    printError(err);
 }
